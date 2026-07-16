788838f3cc4f5344669bbc25b5c80b231b5cf65a
judemcastillo <judemcastillo@gmail.com>
2026-07-16 16:38:25 +0800

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function Home() {
+	return <div className="flex min-h-screen home-wrapper">Home</div>;
+}
